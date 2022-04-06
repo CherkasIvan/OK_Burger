4d0bacc0afdacf0cf3d92ca78fa01f6f3ad044b8
Ivan <distor6on@yandex.ru>
2022-04-06 10:58:52 +0300

    init routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,58 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   redirectTo: '/',
+  //   pathMatch: 'full'
+  // },
+  {
+    path: 'about-page',
+    loadChildren: () =>
+      import('./pages/about-page/about-page.module').then(
+        (m) => m.AboutPageModule
+      ),
+  },
+  {
+    path: 'team-page',
+    loadChildren: () =>
+      import('./pages/team-page/team-page.module').then(
+        (m) => m.TeamPageModule
+      ),
+  },
+  {
+    path: 'orders-page',
+    loadChildren: () =>
+      import('./pages/orders-page/orders-page.module').then(
+        (m) => m.OrdersPageModule
+      ),
+  },
+  {
+    path: 'menu-page',
+    loadChildren: () =>
+      import('./pages/menu-page/menu-page.module').then(
+        (m) => m.MenuPageModule
+      ),
+  },
+  {
+    path: 'events-page',
+    loadChildren: () =>
+      import('./pages/events-page/events-page.module').then(
+        (m) => m.EventsPageModule
+      ),
+  },
+  {
+    path: 'contacts-page',
+    loadChildren: () =>
+      import('./pages/contacts-page/contacts-page.module').then(
+        (m) => m.ContactsPageModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { HeaderModule } from './components/header/header.module';
-import { LogoModule } from './components/logo/logo.module';
 
 import { AppComponent } from './app.component';
 
+import { AboutPageModule } from './pages/about-page/about-page.module';
+import { ContactsPageModule } from './pages/contacts-page/contacts-page.module';
+import { OrdersPageModule } from './pages/orders-page/orders-page.module';
+import { MenuPageModule } from './pages/menu-page/menu-page.module';
+import { EventsPageModule } from './pages/events-page/events-page.module';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [HeaderModule, BrowserModule, AppRoutingModule, LogoModule],
+  imports: [
+    AppRoutingModule,
+    HeaderModule,
+    BrowserModule,
+    RouterModule,
+    AboutPageModule,
+    ContactsPageModule,
+    OrdersPageModule,
+    MenuPageModule,
+    EventsPageModule,
+    ContactsPageModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
