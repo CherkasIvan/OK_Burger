977f9fd86d87f70dc31d430e710608bdeca80f30
Ivan <distor6on@yandex.ru>
2022-04-08 11:50:43 +0300

    add dinamic path to svg

--- src/app/components/social-media-btn/social-media-btn.component.ts
+++ src/app/components/social-media-btn/social-media-btn.component.ts
@@ -1,26 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { SocialMediaBtnService } from 'src/app/service/social-media-btn.service';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-social-media-btn',
   templateUrl: './social-media-btn.component.html',
   styleUrls: ['./social-media-btn.component.scss'],
 })
-export class SocialMediaBtnComponent implements OnInit {
+export class SocialMediaBtnComponent {
   @Input() btnClass: string = '';
   @Input() btnSvg: string = '';
-  public newDoc: any;
-  public wrapper: any = document.createElement('div');
-  // inputElement = document.appendChild(this.wrapper);
 
-  constructor(private socialService: SocialMediaBtnService) {}
-  ngOnInit(): void {
-    this.wrapper.innerHTML = this.btnSvg;
-    let path = this.wrapper.firstChild;
-    this.newDoc = path
-    console.log(path);
-    console.log(this.newDoc);
-    // let d1 = document.getElementsByTagName('a');
-    // return d1.insertAdjacentHTML('afterend', this.btnSvg);
-  }
+  constructor() {}
 }
--- src/app/components/social-media-btn/social-media-btn.module.ts
+++ src/app/components/social-media-btn/social-media-btn.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { SocialMediaBtnComponent } from './social-media-btn.component';
 
 @NgModule({
